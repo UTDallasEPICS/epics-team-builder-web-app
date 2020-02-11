163cc9f38da9b22b958c9ed8d61239a378c54710
Luis Flores <luisflores21095@yahoo.com>
2020-02-11 17:24:21 -0600

    layout

---
+++ epics-team-builder/src/widgets/SetupPage/components/Header.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export default class Header extends React.Component {
+  render() {
+    return <div className="header">HEADER PLACE HOLDER</div>;
+  }
+}
--- epics-team-builder/src/widgets/SetupPage/components/SetupPage.jsx
+++ epics-team-builder/src/widgets/SetupPage/components/SetupPage.jsx
@@ -1,7 +1,22 @@
-import React from 'react';
-
+import React from "react";
+import Header from "../components/Header";
 export default class SetupPage extends React.Component {
   render() {
-    return <div className='setup-page'>Setup</div>;
+    return (
+      <div className="setup-page">
+        <Header />
+        <div className="setup-grid">
+
+          <div className="File-Uploader">{/*Component goes here*/}</div>
+
+          <div className="Manual-project-Assignment"></div>
+
+          <div className="Manually-Assigned-Students"></div>
+
+          <div className="Build-Team"></div>
+          
+        </div>
+      </div>
+    );
   }
 }
