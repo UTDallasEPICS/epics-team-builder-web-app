d53361cc522989be055fad87f8195fa7c3acb1a3
William Truong <Williamtruong09@gmail.com>
2020-02-12 10:40:20 -0600

    made upload button functional, having issue sending it to upload folders, http proxy error

--- epics-team-builder/src/widgets/SetupPage/components/FileUploaders.jsx
+++ epics-team-builder/src/widgets/SetupPage/components/FileUploaders.jsx
@@ -4,15 +4,18 @@ import Axios from 'axios';
 const FileUpload = () =>{ // File Uploader using ReactHooks
     // Const[] is used to display the file name once it is uploaded 
 
+    // ONCHANGE FUNCTIONS
     const[file,setFile] = useState('') ;
     const[filename,setFilename] = useState('Choose File') ;
     const[uploadedFile, setUploadedFile] = useState({});
 
+    //If the file is changed, this will display the file name in the box
     const onChange = e => {
         setFile(e.target.files[0]) ;
         setFilename(e.target.files[0].name) ; 
     };
 
+    // Once the user hits upload
     const onSubmit = async e =>{
         e.preventDefault();
         const formData = new FormData(); 
@@ -26,13 +29,21 @@ const FileUpload = () =>{ // File Uploader using ReactHooks
                     'Content-Type':'multipart/form-data'
                 }
             }) ;
-
+         const {filename, filePath} = res.data ; 
+         setUploadedFile({filename,filePath}) ;
         }catch(err){
-
+         if(err.response.status === 500){
+             console.log('Server Issue') ;
+         }
+         else{
+             console.log(err.response.data.msg) ;
+         }
         }
     } ; // Function submit the file to the server * MAY NOT BE NECESSARY *
 
     return(
+        // FORM 1 = PROJECT FILE UPLOAD 
+        // FORM 2 = STUDENT FILE UPLOAD  
         <Fragment> 
             <form onSubmit = {onSubmit}> 
                 <div className="custom-file mb-4">
@@ -45,7 +56,7 @@ const FileUpload = () =>{ // File Uploader using ReactHooks
             <form>
                 <div className="custom-file mb-4">
                     <input type="file" className="custom-file-input" id="customFile" onChange = {onChange}/>
-    <label className="custom-file-label" htmlFor="customFile">{filename}</label>
+              <label className="custom-file-label" htmlFor="customFile">{filename}</label>
                 </div>
                 <input type="submit" value="Upload" className = "btm btn-primary btn-block mt-4"/>
             </form>
