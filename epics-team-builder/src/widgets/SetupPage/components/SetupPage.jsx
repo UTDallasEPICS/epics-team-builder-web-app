2577c4aae736887fb9e015b6f9c647f44fb39983
William Truong <Williamtruong09@gmail.com>
2020-02-12 13:18:58 -0600

    minor changeto css

--- epics-team-builder/src/widgets/SetupPage/components/SetupPage.jsx
+++ epics-team-builder/src/widgets/SetupPage/components/SetupPage.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import Header from '../../common/Header';
-import FileUploaders from './FileUploaders'
+import FileUploaders from './FileUploaders';
 export default class SetupPage extends React.Component {
   render() {
     return (
@@ -8,8 +8,9 @@ export default class SetupPage extends React.Component {
         <Header />
         <div className="setup-grid">
           {/* Make sure to put these divs in their respective components when made */}
-          <div className="file-uploader">{/*Component goes here*/}
-          <FileUploaders />
+          <div className="file-uploader">
+            {/*Component goes here*/}
+            <FileUploaders />
           </div>
 
           <div className="manual-project-assignment"></div>
