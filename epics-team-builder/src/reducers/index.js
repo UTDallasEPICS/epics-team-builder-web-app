d127dc7b18a3c0936f27435ef3dcfc6d81068aa3
Matthew Nevle <Manevle16@gmail.com>
2020-02-14 02:52:15 -0600

    setup initial reducer for Setup Page

--- epics-team-builder/src/reducers/index.js
+++ epics-team-builder/src/reducers/index.js
@@ -1,3 +1,11 @@
 import { combineReducers } from 'redux';
+import { createSelector } from 'reselect';
+import setupPageReducer from './setupPageReducer';
 
-export default combineReducers({});
+export default combineReducers({
+  setupPage: setupPageReducer
+});
+
+export const getStudents = state => state.setupPage.students;
+export const getProjects = state => state.setupPage.projects;
+export const getProjectsByStudentId = state => state.setupPage.projectsByStudentId;
