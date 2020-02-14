89ff21e50880e002a5e12966163e52e641a6910d
unknown <Williamtruong09@gmail.com>
2020-02-14 11:38:06 -0600

    consoloe.log

--- epics-team-builder/src/widgets/SetupPage/components/FileUploaders.jsx
+++ epics-team-builder/src/widgets/SetupPage/components/FileUploaders.jsx
@@ -1,49 +1,47 @@
 import React, { Fragment } from 'react';
-import readXlsxFile from 'read-excel-file' ;
-
+import readXlsxFile from 'read-excel-file';
 
 const FileUpload = () => {
   const projectSchema = [
     {
-      'PROJECT NAME':{
+      'PROJECT NAME': {
         prop: 'projectName',
-        type:String
+        type: String
       },
-      'Returning(Y/N)':{
-        prop:'returningProject',
-        type:Boolean
+      'Returning(Y/N)': {
+        prop: 'returningProject',
+        type: Boolean
       },
-      'SKILL 1':{
-        prop:'skill1',
-        type:String
+      'SKILL 1': {
+        prop: 'skill1',
+        type: String
       },
-      'SKILL 2':{
-        prop:'skill2',
-        type:String
+      'SKILL 2': {
+        prop: 'skill2',
+        type: String
       },
-      'SKILL 3':{
-        prop:'skill3',
-        type:String
+      'SKILL 3': {
+        prop: 'skill3',
+        type: String
       }
-      
     }
-  ]
+  ];
 
-  const onChangeProjects = e =>{
-    readXlsxFile( document.getElementById('input').files[0], {projectSchema}).then((rows,errors) => {
-      
-      console.log(rows) ;
+  const onChangeProjects = e => {
+    readXlsxFile(document.getElementById('input').files[0], {
+      projectSchema
+    }).then((rows, errors) => {
+      console.log(rows);
+    });
+  };
 
-      })
-  }
-
-  const onChangeStudent = e =>{
-    readXlsxFile( document.getElementById('studentinput').files[0]).then((rows,errors) => {
-     
-      
-
-      })
-  }
+  const onChangeStudent = e => {
+    readXlsxFile(
+      document.getElementById('studentinput').files[0]
+    ).then((rows, errors) => {
+      console.log(rows);
+    });
+  };
 
   // Once the user hits upload
 
@@ -51,11 +49,19 @@ const FileUpload = () => {
     // FORM 1 = PROJECT FILE UPLOAD
     // FORM 2 = STUDENT FILE UPLOAD
     <Fragment>
+      <input
+        type="file"
+        id="input"
+        accept=".xls, .xlsx"
+        onChange={onChangeProjects}
+      />
 
-      <input type = "file" id = "input" accept = ".xls, .xlsx" onChange = {onChangeProjects}/>
-      
-      <input type = "file" id = "studentinput" accept = ".xls,.xlsx" onChange = {onChangeStudent} />
-
+      <input
+        type="file"
+        id="studentinput"
+        accept=".xls,.xlsx"
+        onChange={onChangeStudent}
+      />
     </Fragment>
   );
 };
