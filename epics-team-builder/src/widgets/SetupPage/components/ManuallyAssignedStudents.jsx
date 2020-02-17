8cca75fb23c17995e20ca3071d30d8054c21b344
Luis Flores <luisflores21095@yahoo.com>
2020-02-16 21:04:56 -0600

    Added the table and some features

--- epics-team-builder/src/widgets/SetupPage/components/ManuallyAssignedStudents.jsx
+++ epics-team-builder/src/widgets/SetupPage/components/ManuallyAssignedStudents.jsx
@@ -1,10 +1,67 @@
-import React from 'react';
+import React from "react";
+import { Card, Table, Button } from "react-bootstrap";
 
 export default class ManuallyAssignedStudents extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: [
+        { firstName: "luis", lastName: "flores", userName: "dfasd" },
+        {
+          firstName: "John",
+          lastName: "Reno",
+          userName: "oowsd"
+        },
+        {
+          firstName: "Jhon",
+          lastName: "Phillip",
+          userName: "plsk"
+        }
+      ]
+    };
+  }
+
   render() {
     return (
-      <div className="manuallyassignedstudents">
-          hello
+      <div style={{ height: "100%", width: "100%" }}>
+        <Card
+          style={{
+            height: "70%",
+            width: "80%",
+            margin: "5% auto",
+            overflow: "auto"
+          }}
+        >
+          <Table striped bordered hover>
+            <thead>
+              <tr>
+                <th>Delete</th>
+                <th>First Name</th>
+                <th>Last Name</th>
+                <th>Username</th>
+              </tr>
+            </thead>
+            <tbody>
+              {this.state.data.map((listValue, index) => {
+                return (
+                  <tr key={index}>
+                    <td>
+                      <input type="checkbox" name="box" value={index}></input>
+                    </td>
+                    <td>{listValue.firstName}</td>
+                    <td>{listValue.lastName}</td>
+                    <td>{listValue.userName}</td>
+                  </tr>
+                );
+              })}
+            </tbody>
+          </Table>
+        </Card>
+        <div style={{ display: "flex", justifyContent: "center" }}>
+          <Button variant="primary" size="md">
+            Large button
+          </Button>
+        </div>
       </div>
     );
   }
