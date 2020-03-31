1911f2e0ed8a207fbfa8c9aab08b6bf516c67d9c
Matthew Nevle <Manevle16@gmail.com>
2020-03-31 10:15:57 -0500

    remove unecessary divs and css

--- epics-team-builder/src/widgets/TeamBuilderPage/components/TeamBuilderPage.jsx
+++ epics-team-builder/src/widgets/TeamBuilderPage/components/TeamBuilderPage.jsx
@@ -23,17 +23,17 @@ function TeamBuilderPage(props) {
   };
 
   const renderTopSection = () => (
-    <Row className='py-3'>
-      <Col xs={12} md={2} className='p-md-4'>
-        <button onClick={props.switchToSetup()} className='px-5 py-2 green'>
+    <Row className="py-3">
+      <Col xs={12} md={2} className="p-md-4">
+        <button onClick={props.switchToSetup()} className="px-5 py-2 green">
           Go Back
         </button>
       </Col>
-      <Col xs={12} md={7} className=' text-center'>
-        <div className='font-weight-bolder text-center py-2'>
+      <Col xs={12} md={7} className=" text-center">
+        <div className="font-weight-bolder text-center py-2">
           <h3>Attribute Importance</h3>
         </div>
-        <div className='d-md-flex md-flex-row justify-content-center'>
+        <div className="d-md-flex md-flex-row justify-content-center">
           <CheckBox />
         </div>
       </Col>
@@ -41,33 +41,27 @@ function TeamBuilderPage(props) {
   );
 
   const renderLoading = () => (
-    <div style={{ height: '50vh' }} className='d-flex justify-content-center align-items-center'>
-      <div>
-        <Spinner animation='border' role='status' size='lg'></Spinner>
-      </div>
+    <div style={{ height: '50vh' }} className="d-flex justify-content-center align-items-center">
+      <Spinner animation="border" role="status" size="lg"></Spinner>
     </div>
   );
 
   const renderTeamCombinations = () => (
-    <div className='py-2' style={{ height: 'auto' }}>
-      <div>
-        <div className='font-weight-bolder text-center py-1'>
-          <h4>Team Combinations</h4>
-        </div>
-        <div>
-          <DisplayTeamCombinations
-            teamCombos={teamCombos}
-            selectCombination={selectCombination}
-            regrenerateTeam={regrenerateTeam}
-          />
-        </div>
+    <div className="py-2" style={{ height: 'auto' }}>
+      <div className="font-weight-bolder text-center py-1">
+        <h4>Team Combinations</h4>
       </div>
+      <DisplayTeamCombinations
+        teamCombos={teamCombos}
+        selectCombination={selectCombination}
+        regrenerateTeam={regrenerateTeam}
+      />
     </div>
   );
 
   const renderViewTeam = () => (
     <div>
-      <div className='font-weight-bolder text-center'>
+      <div className="font-weight-bolder text-center">
         <h4>View Team</h4>
       </div>
       <div>{/* Make sure to put these divs in their respective components when made */}</div>
@@ -76,7 +70,7 @@ function TeamBuilderPage(props) {
 
   const renderTeamInformation = () => (
     <div>
-      <div className='font-weight-bolder text-center'>
+      <div className="font-weight-bolder text-center">
         <h4>Team Information</h4>
       </div>
       <div>{/* Make sure to put these divs in their respective components when made */}</div>
@@ -84,17 +78,17 @@ function TeamBuilderPage(props) {
   );
 
   return (
-    <div className='team-builder-page'>
+    <div className="team-builder-page">
       <Header />
       {renderTopSection()}
       <Row>
-        <Col xs={12} md={4} className='bg-light'>
+        <Col xs={12} md={4} className="bg-light">
           {loading ? renderLoading() : renderTeamCombinations()}
         </Col>
-        <Col xs={12} md={4} className='bg-success'>
+        <Col xs={12} md={4} className="bg-success">
           {renderViewTeam()}
         </Col>
-        <Col xs={12} md={4} className='bg-info'>
+        <Col xs={12} md={4} className="bg-info">
           {renderTeamInformation()}
         </Col>
       </Row>
